16b98d4376afb0fb40171a4f8743ff212ff87954
Abdullah mohamed <112867836+mohamed12345abdullah@users.noreply.github.com>
2026-02-15 20:54:27 +0200

    del app .listen

--- index.js
+++ index.js
@@ -60,9 +60,12 @@ app.use((err, req, res, next) => {
 
 
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`)
-    console.log(`https//localhost:${process.env.PORT}`);
+// app.listen(process.env.PORT, () => {
+//     console.log(`Server is running on port ${process.env.PORT}`)
+//     console.log(`https//localhost:${process.env.PORT}`);
     
-})
+// })
 
+
+
+export default app;
